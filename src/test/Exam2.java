0388ec750ac283ab13d1fd3d9801a6c3c6527d35
jang48 <jjangmi4843@gmail.com>
2023-09-11 17:18:27 +0900

    Exam2 & Exam3

---
+++ src/test/Exam2.java
@@ -0,0 +1,39 @@
+package test;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        //높이를 입력해주세요 : 3
+            for (int j = 1; j > 0; j--) {
+                System.out.print(" ");
+                System.out.print(" ");
+                for (int k = 1; k < 2; k++) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+                System.out.println();
+
+                System.out.print(" ");
+                for (int k = 1; k < 4; k++) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+                System.out.println();
+
+                for (int k = 1; k < 6; k++) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+        }
+
+
+        System.out.println();
+
+        //높이를 입력해주세요 : 5
+
+
+
+
+        //높이를 입력해주세요 : 7
+
+    }
+}
